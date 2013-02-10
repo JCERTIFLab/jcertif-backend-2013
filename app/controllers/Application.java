8ea1e00493cf5728cb084063cabfa60941923799
OlivierFixe <oliviermbieleu@gmail.com>
2013-02-10 15:18:20 +0100

    Un peu compliqué à mettre en place avec intellij et lent à démarrer

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,10 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-      //views.html.index.render("Toles")
-
-    //return ok(index.render("Your new application is ready.") );
-      return ok("Your new Application is ready");
+    return ok(index.render("Your new application is ready."));
   }
   
 }
