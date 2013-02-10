cf5450eab0c5a7f27250ad9fa86f78262a9dd36a
OlivierFixe <oliviermbieleu@gmail.com>
2013-02-10 13:13:24 +0100

    Test de commit. J'ai des erreurs de compilation. Le package views.html.index n'existe plus. Quelle version de play? Compilation avec intellij. A revoir

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,7 @@ public class Application extends Controller {
   public static Result index() {
       //views.html.index.render("Toles")
 
-    //return ok(index.render("Your new application is ready."));
+    //return ok(index.render("Your new application is ready.") );
       return ok("Your new Application is ready");
   }
   
