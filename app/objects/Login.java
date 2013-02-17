02ad47477d9c6cd43108609fb0d688ebdb076e0b
OlivierFixe <oliviermbieleu@gmail.com>
2013-02-17 04:40:32 +0100

    Permettre aux objets d'avoir leur représentation en BasicDBObject => objectif : Sérialisation via JSON de MongoDB

--- app/objects/Login.java
+++ app/objects/Login.java
@@ -1,5 +1,7 @@
 package objects;
 
+import com.mongodb.BasicDBObject;
+
 public class Login {
     private String email;
     private String password;
@@ -18,5 +20,12 @@ public class Login {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public BasicDBObject toDBObject(){
+        BasicDBObject basicDBObject = new BasicDBObject();
+        basicDBObject.put("email", email);
+        basicDBObject.put("password", password);
+        return basicDBObject;
     }
 }
