53910a50472eb4c8b46b13e962c0cdb102aef06b
Rossi Oddet <rossi.oddet@gmail.com>
2013-02-20 20:59:18 +0100

    test intégration continue

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -3,7 +3,7 @@ package controllers;
 import com.mongodb.util.JSON;
 import objects.access.LoginDB;
 import play.mvc.Result;
-
+ 
 public class LoginController extends AbstractController {
  
     public static Result logins() {
