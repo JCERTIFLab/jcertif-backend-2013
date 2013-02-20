b6e8666f3495fe48a883ccc6da1382d292e973e5
Rossi Oddet <rossi.oddet@gmail.com>
2013-02-20 20:49:57 +0100

    test intégration continue

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -5,7 +5,7 @@ import objects.access.LoginDB;
 import play.mvc.Result;
 
 public class LoginController extends AbstractController {
-
+ 
     public static Result logins() {
         allowCrossOriginJson();
         return ok(JSON.serialize(LoginDB.loginDB.list()));
