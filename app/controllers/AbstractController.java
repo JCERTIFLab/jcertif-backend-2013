738fcece1b2a760c77ee77907df6d851169cf9a8
Rossi Oddet <rossi.oddet@gmail.com>
2013-02-25 23:56:32 +0100

    Réalisation des tests fonctionnels pour les services REST : "Liste des niveaux de partenariat" et "Liste des categories". Activation de l'autorisation du cross origin pour la méthode POST

--- app/controllers/AbstractController.java
+++ app/controllers/AbstractController.java
@@ -6,7 +6,7 @@ public abstract class AbstractController extends Controller {
 
     protected static void allowCrossOriginJson() {
         response().setHeader("Access-Control-Allow-Origin", "*");
-        response().setHeader("Access-Control-Allow-Methods", "GET");
+        response().setHeader("Access-Control-Allow-Methods", "GET,POST");
         response().setHeader("Access-Control-Allow-Headers", "Content-Type");
         response().setHeader("Content-Type", "application/json; charset=utf-8");
 
