9a175b415fd98a68751c196a7fe84d06024004d7
bashizip <bashizip@gmail.com>
2013-03-11 02:29:36 +0100

    Nouveau service : Login d'un participant ( vérification des paramètes de connexion)

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -1,14 +1,71 @@
 package controllers;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.util.JSON;
+import com.mongodb.util.JSONParseException;
+
+import models.exception.JCertifException;
+import models.objects.Participant;
 import models.objects.access.LoginDB;
+import models.objects.access.ParticipantDB;
+import models.util.Tools;
 import play.mvc.Result;
+import play.mvc.Http.RequestBody;
 
 public class LoginController extends AbstractController {
 
-    public static Result logins() {
-        allowCrossOriginJson();
-        return ok(JSON.serialize(LoginDB.getInstance().list()));
-    }
+	public static Result logins() {
+		allowCrossOriginJson();
+		return ok(JSON.serialize(LoginDB.getInstance().list()));
+	}
 
+	/**
+	 * Login action
+	 * 
+	 * @param json  payload
+	 *          
+	 * @param
+	 * @return
+	 */
+	public static Result login() {
+	
+		allowCrossOriginJson();
+
+		RequestBody requestBody = request().body();
+		try {
+			Tools.verifyJSonRequest(requestBody);
+		} catch (JCertifException e) {
+			return badRequest(e.getMessage());
+		}
+
+		String loginObjInJSONForm = request().body().asJson().toString();
+
+		BasicDBObject params;
+		try {
+			params = (BasicDBObject) JSON.parse(loginObjInJSONForm);
+		} catch (JSONParseException exception) {
+			return badRequest(loginObjInJSONForm);
+		}
+		String email = params.getString("email");
+		String password = params.getString("password");
+
+		Participant participant;
+		try {
+			participant = ParticipantDB.getInstance().get(email);
+		} catch (JCertifException jcertifException) {
+			return internalServerError(jcertifException.getMessage());
+		}
+
+		if (participant == null) {
+			return internalServerError(JSON
+					.serialize("Participant with email \"" + email
+							+ "\" does not exist"));
+		}
+		if (!(participant.getPassword().equals(password))){
+			return internalServerError(JSON
+					.serialize("Login failed!, Username or Password invalid"));
+
+		}
+		return ok(JSON.serialize(participant.toBasicDBObject()));
+	}
 }
