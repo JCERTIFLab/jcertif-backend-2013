44589850abf230b6e2959584f9b896aaa4d0460b
Rossi Oddet <rossi.oddet@gmail.com>
2013-03-15 23:39:05 +0100

    Ajout du plugin Jacoco pour le calcul de la couverture du code par les tests du projet.

--- project/Build.scala
+++ project/Build.scala
@@ -1,19 +1,25 @@
 import sbt._
 import Keys._
 import play.Project._
+import de.johoop.jacoco4sbt._
+import JacocoPlugin._
 
 object ApplicationBuild extends Build {
 
   val appName = "jcertif-backend"
   val appVersion = "1.0-SNAPSHOT"
 
+  lazy val s = Defaults.defaultSettings ++ Seq(jacoco.settings:_*)
+
   val appDependencies = Seq(
     "org.mongodb" % "mongo-java-driver" % "2.10.1",
     javaCore,"com.typesafe" %% "play-plugins-mailer" % "2.1.0"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here
+  val main = play.Project(appName, appVersion, appDependencies, settings = s).settings(
+    parallelExecution     in jacoco.Config := false,
+    jacoco.reportFormats  in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8")),
+    jacoco.excludes       in jacoco.Config := Seq("views.*", "controllers.Reverse*", "controllers.javascript.*", "controllers.ref.*", "Routes*")
   )
 
 }
