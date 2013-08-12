96af83364523d3341fc707f3cc2ddfb0ca6f9db3
Martial SOMDA <martialsomda@gmail.com>
2013-08-13 00:38:04 +0200

    Workaround for junit tests not executing anymore since the upgrade to play 2.1.3 (see https://github.com/playframework/playframework/issues/1437)

--- project/Build.scala
+++ project/Build.scala
@@ -14,12 +14,19 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     "org.mongodb" % "mongo-java-driver" % "2.10.1",
     javaCore,"com.typesafe" %% "play-plugins-mailer" % "2.1.0",
-    "commons-lang" % "commons-lang" % "2.4",
-    "commons-codec" % "commons-codec" % "1.3"
+    "commons-lang" % "commons-lang" % "2.4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies, settings = s).settings(
-    resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns),
+
+    testOptions in Test ~= { args =>
+    for {
+        arg <- args
+        val ta: Tests.Argument = arg.asInstanceOf[Tests.Argument]
+        val newArg = if(ta.framework == Some(TestFrameworks.JUnit)) ta.copy(args = List.empty[String]) else ta
+      } yield newArg
+    },
+
     parallelExecution     in jacoco.Config := false,
     jacoco.reportFormats  in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8")),
     jacoco.excludes       in jacoco.Config := Seq("views.*", "controllers.Reverse*", "controllers.javascript.*", "controllers.ref.*", "Routes*")
