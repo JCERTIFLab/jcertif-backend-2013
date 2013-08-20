44d286638a0996b13c8871330c27c5d46905f1d9
Rossi Oddet <rossi.oddet@gmail.com>
2013-08-21 00:45:00 +0200

    [AUTHENTIFICATION] Permission d'avoir les infos d'authentification dans l'url de la requête dans le cas d'un POST, PUT, DELETE etc... Ce privilege n'est plus reserve au GET :)

--- app/controllers/DefaultAuthenticator.java
+++ app/controllers/DefaultAuthenticator.java
@@ -13,51 +13,49 @@ import play.mvc.Security.Authenticator;
 
 /**
  * @author Martial SOMDA
- *
  */
 public class DefaultAuthenticator extends Authenticator {
 
-	@Override
-	public String getUsername(Context context) {
-		String accessToken = null;
-		String provider = null;
-		String email = null;
- 
-		if ("GET".equals(context.request().method())) {
-			accessToken = context.request().getQueryString("access_token");
-			provider = context.request().getQueryString("provider");
-			email = context.request().getQueryString("user");
-		} else {
-			JsonNode node = context.request().body().asJson().findPath("access_token");
-			if(null != node){
-				accessToken = node.getTextValue();
-			}
-			node = context.request().body().asJson().findPath("provider");
-			if(null != node){
-				provider = node.getTextValue();
-			}	
-			node = context.request().body().asJson().findPath("user");
-			if(null != node){
-				email = node.getTextValue();
-			}	
-		}
-		
-		if(Tools.isBlankOrNull(accessToken) ||
-				Tools.isBlankOrNull(provider)){
-			return null;
-		}
+    @Override
+    public String getUsername(Context context) {
+        // Par défaut recherche des infos d'authentification dans l'URL
+        String accessToken = context.request().getQueryString("access_token");
+        String provider = context.request().getQueryString("provider");
+        String email = context.request().getQueryString("user");
 
-		TokenCheck check = TokenChecksFactoy.getInstance().getCheck(provider);
-		if(check.isValid(accessToken, email)){
-			Logger.info("token is valid");
-			return accessToken;
-		}else{
-			return null;
-		}		
-	}
-	
-	@Override
-	public Result onUnauthorized(Context context) {
-		return unauthorized("Operation not allowed for non-authenticated people");
-	}
+        // Si les infos d'authentification ne sont pas trouvées,
+        // on s'autorise à les chercher dans le corps de la requête
+        // Le cas du GET est écarté car par principe la requête ne doit pas contenir de corps.
+        if ((Tools.isBlankOrNull(accessToken) || Tools.isBlankOrNull(provider)) && !"GET".equals(context.request().method())) {
+            JsonNode node = context.request().body().asJson().findPath("access_token");
+            if (null != node) {
+                accessToken = node.getTextValue();
+            }
+            node = context.request().body().asJson().findPath("provider");
+            if (null != node) {
+                provider = node.getTextValue();
+            }
+            node = context.request().body().asJson().findPath("user");
+            if (null != node) {
+                email = node.getTextValue();
+            }
+        }
+
+        if (Tools.isBlankOrNull(accessToken) || Tools.isBlankOrNull(provider)) {
+            return null;
+        }
+
+        TokenCheck check = TokenChecksFactoy.getInstance().getCheck(provider);
+        if (check.isValid(accessToken, email)) {
+            Logger.info("token is valid");
+            return accessToken;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Result onUnauthorized(Context context) {
+        return unauthorized("Operation not allowed for non-authenticated people");
+    }
 }
